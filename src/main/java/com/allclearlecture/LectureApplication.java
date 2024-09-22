5c9ac9527dca712baf7e3e85d36bf539070d4b24
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-09-22 18:23:43 +0900

    fix: move package #13

--- src/main/java/com/allclearlecture/LectureApplication.java
+++ src/main/java/com/allclearlecture/LectureApplication.java
@@ -1,4 +1,4 @@
-package com.allclearlecture.lecture;
+package com.allclearlecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
