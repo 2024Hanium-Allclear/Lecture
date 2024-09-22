8a69a1fbe2c3e7d674a3a6aa6ed3fe06dab64933
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-09-22 18:17:05 +0900

    feat: add College, Department, Lecture , Professor Entity #13

---
+++ src/main/java/com/allclearlecture/domain/lecture/entity/Professor.java
@@ -0,0 +1,32 @@
+package com.allclearlecture.domain.lecture.entity;
+
+
+import com.allclearlecture.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Professor extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "professor_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    private String name;
+
+    private String lab;
+
+    public Professor(Long id) {
+        this.id = id;
+    }
+}
