efa0d15f9fc4c3988b3e698a6e6a738974977755
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-09-22 18:12:16 +0900

    setting: ApiResponse, BaseEntity, BaseErrorCode, GeneralErrorCode, GeneralException, GlobalExceptionHandler, initData,TestController

---
+++ src/main/java/com/allclearlecture/global/common/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.allclearlecture.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @Column(nullable = false)
+    private boolean delStatus;
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
